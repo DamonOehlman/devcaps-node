3307af0b677414248d8956f4727cf3c56e64e722
Damon Oehlman <damon.oehlman@gmail.com>
2011-09-30 12:36:47 +1000

    Implemented definitions and making more friendly

--- src/detector.js
+++ src/detector.js
@@ -1,49 +1,41 @@
 //= lib/modernizr.custom
 
-// github://tdd/cookies-js-helper/src/cookies
-
-(function() {
+DETECTOR = function(opts) {
+    // initialise options
+    opts = opts || {};
+    
     var frmDetector = document.getElementById('detector'),
         elDevCaps = document.getElementById('devcaps'),
         tests = {
-            'DSW': (screen || window).width,
-            'DSH': (screen || window).height
-        },
-        mappings = {
-            canvas: 'CVS',
-            flexbox: 'FBX',
-            csstransforms: 'T2D',
-            csstransforms3d: 'T3D',
-            localstorage: 'LST'
+            'SCR': (screen || window).width + 'x' + (screen || window).height
         },
         key,
         test,
         output = [],
-        opts = typeof devcapOpts != 'undefined' ? devcapOpts.split(',') : [],
         reReset = /(?:\?|\&)reset(?=\&|$)/i;
         
-    // iterate through the modernizr mappings and if defined, add to the tests as a 1
-    for (key in mappings) {
-        if (Modernizr[key]) {
-            tests[mappings[key]] = 1;
-        } // if
-    } // for
-    
     // iterate through the target tests
-    for (var ii = 0; ii < opts.length; ii++) {
-        key = opts[ii];
-        test = tests[key];
+    for (key in opts) {
+        var keyVal = opts[key];
         
-        if (typeof test == 'function') {
-            
-        }
-        else {
-            output.push(key + test);
-        } // if..else
+        if (typeof keyVal == 'undefined') {
+            test = tests[key];
+
+            if (typeof test == 'function') {
+
+            }
+            else if (typeof test != 'undefined') {
+                keyVal = test;
+            } // if..else
+        } // if
+        
+        if (typeof keyVal != 'undefined') {
+            output.push(key + (keyVal === true ? 1 : keyVal));
+        } // if
     }  // for
     
     // update the device capabilities
     elDevCaps.value = output.join('_');
     frmDetector.action = document.location.href.replace(reReset, '');
     frmDetector.submit();
-})();
+};
--- test/server.js
+++ test/server.js
@@ -4,10 +4,12 @@ var connect = require('connect'),
     
 app.use(connect.cookieParser());
 app.use(connect.bodyParser());
-app.use(devcaps());
+app.use(devcaps.checkFor());
 
 app.use(function(req, res, next) {
-    res.end(JSON.stringify(req.devcaps));
+    var capsData = JSON.stringify(req.devcaps);
+    
+    res.end('<html><h2>Device Caps (Server Generated HTML)</h2><p>' + capsData + '</p><h2>Actions</h2><p><a href="/random">test</a>&nbsp;<a href="/?reset">reset</a></p></html>');
 });
 
 app.listen(3000);
